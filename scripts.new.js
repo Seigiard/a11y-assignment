edbb99068b33155ed778ef8933781efbb7545d01
Andrew Borisenko <seigiard@gmail.com>
2024-11-02 17:13:13 +0100

    update product

--- scripts.new.js
+++ scripts.new.js
@@ -16,19 +16,52 @@ function handleFromSubmitAndShowPopover(event) {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  const inputsQuantity = document.querySelectorAll('.input-quantity');
+  const inputsQuantity = document.querySelectorAll('.productForm-quantity');
   inputsQuantity.forEach((input) => {
-    const inputField = input.querySelector('.input-quantity__field');
-    const inputBtnIncrease = input.querySelector('.input-quantity__btn[data-action=increase]');
-    const inputBtnDecrease = input.querySelector('.input-quantity__btn[data-action=decrease]');
+    const inputField = input.querySelector('.productForm-quantity__field');
+    const inputQuantityText = document.getElementById(inputField.getAttribute('data-sync-with'))
+
+    const inputBtnIncrease = input.querySelector('.productForm-quantity__btn[data-action=increase]');
+    const inputBtnDecrease = input.querySelector('.productForm-quantity__btn[data-action=decrease]');
     inputBtnIncrease.addEventListener('click', () => {
       const initialValue = inputField.value * 1;
       inputField.value = initialValue + 1;
+      syncState()
     });
     inputBtnDecrease.addEventListener('click', () => {
       const initialValue = inputField.value * 1;
       if (initialValue > 1) inputField.value = initialValue - 1;
+      syncState()
     });
+
+    inputField.addEventListener('change', syncState)
+    syncState()
+
+    function syncState() {
+      const value = inputField.value * 1;
+      const items = value === 1 ? 'item' : 'items';
+      inputQuantityText.textContent = `${value} ${items}`;
+
+      if (value === 1) {
+        inputBtnDecrease.setAttribute('disabled', true);
+      } else {
+        inputBtnDecrease.removeAttribute('disabled');
+      }
+    }
+  });
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+  const radioButtons = document.querySelectorAll('#color-select input');
+  radioButtons.forEach((input) => {
+    const syncedElement = document.getElementById(input.getAttribute('data-sync-with'))
+
+    input.addEventListener('change', syncState)
+
+    function syncState(e) {
+      const value = e.target.getAttribute('aria-label') || e.target.value;
+      syncedElement.textContent = `Color - ${value}`;
+    }
   });
 })
 
@@ -37,4 +70,18 @@ window.addEventListener("DOMContentLoaded", () => {
   menuToggle.getElementsByTagName('input')[0].addEventListener('change', (e => {
     menuToggle.setAttribute('aria-label', e.target.checked ? 'Collapse menu' : 'Expand menu')
   }))
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+  const popovers = document.querySelectorAll('[popover]');
+
+  function handleFocusOnOpen(element) {
+      const focusableElements = element.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+      const firstFocusableElement = focusableElements[0];
+      (firstFocusableElement || element).focus();
+  }
+
+  popovers.forEach((popoverElement) => {
+      popoverElement.addEventListener("toggle", ({newState}) => { newState === 'open' && handleFocusOnOpen(popoverElement) });
+  });
 })
